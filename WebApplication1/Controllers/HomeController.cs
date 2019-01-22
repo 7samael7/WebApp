5d5c30e65f4c35e43f371ebef0d0fcf02956827d
seb hruby <sebastianhruby@gmail.com>
2019-01-22 10:12:50 +0100

    Adding EmailSender

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmailSender emailSender;
+
+        internal HomeController(IEmailSender emailSender) => this.emailSender = emailSender;
+
+        public IActionResult Index()
+        {
+            emailSender.SendEmail("pavel_bory@plus4u.net", "Hello from ASP.NET Core");
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contanct.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Services/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public void SendEmail(string email, string text)
+        {
+            Console.WriteLine($"Sending email to: {email} with text: {text}");
+        }
+    }
+}
